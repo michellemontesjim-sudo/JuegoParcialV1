897003e4abe0fb614611f3cd9484068c856643f5
angelesAnchico07 <angeles.anchico@uao.edu.co>
2025-10-14 22:53:56 -0500

    animaciones

---
+++ JuegoParcialUnity/Assets/ScriptsFinal/Scripts/Cofre.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cofre : MonoBehaviour
+{
+    public int CantidadOro = 100;
+    private Animator animator;
+    private bool abierto = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            abierto = !abierto; // alterna abrir/cerrar
+            animator.SetBool("IsOpen", abierto);
+
+            if (abierto)
+                Debug.Log($"Has recibido {CantidadOro} de oro!");
+        }
+    }
+}
