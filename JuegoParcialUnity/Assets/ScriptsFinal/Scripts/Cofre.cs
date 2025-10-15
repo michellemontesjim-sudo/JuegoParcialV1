70ee96ddb2878e7024f849f67f06644688d079ff
angelesAnchico07 <angeles.anchico@uao.edu.co>
2025-10-15 08:47:40 -0500

    diálogo del cofre

--- JuegoParcialUnity/Assets/ScriptsFinal/Scripts/Cofre.cs
+++ JuegoParcialUnity/Assets/ScriptsFinal/Scripts/Cofre.cs
@@ -1,20 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
+using TMPro; 
 
 public class Cofre : MonoBehaviour
 {
     public Animator animator;
     private bool jugadorCerca = false;
     public int oro = 100;
+    public GameObject panelDialogo;
+
+
+    public TextMeshProUGUI textoDialogo;
+
+    private bool cofreAbierto = false;
+
+    private void Start()
+    {
+        panelDialogo.SetActive(false);
+    }
 
     void Update()
     {
-        // Si el jugador está cerca y presiona la tecla E, se abre el cofre
         if (jugadorCerca && Input.GetKeyDown(KeyCode.E))
         {
-            animator.SetBool("IsOpen", true);
-            Debug.Log("Has recibido " + oro + " de oro!");
+            if (!cofreAbierto)
+            {
+                animator.SetBool("IsOpen", true);
+                textoDialogo.text = "¡Has recibido " + oro + " de oro!";
+                cofreAbierto = true;
+            }
+            else
+            {
+                textoDialogo.text = "El cofre ya está vacío...";
+            }
         }
     }
 
@@ -23,7 +43,8 @@ public class Cofre : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             jugadorCerca = true;
-            Debug.Log("Presiona E para abrir el cofre");
+            panelDialogo.SetActive(true);
+            textoDialogo.text = "Presiona E para abrir el cofre";
         }
     }
 
@@ -32,6 +53,7 @@ public class Cofre : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             jugadorCerca = false;
+            panelDialogo.SetActive(false);
         }
     }
 }
